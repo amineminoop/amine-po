4d92bb281f0aaad2b34b867c4fe04ced403afd2f
amineminoop <aminemino12020@gmail.com>
2022-07-21 16:58:44 +0100

    my first change

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>amine</h1>
+
+     
+
+    </div>
+  )
+}
